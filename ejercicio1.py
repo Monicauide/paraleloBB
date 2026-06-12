ea1d2626d734c856c40de42903d25c1ede749df5
Monicauide <monica.salazar8191@utc.edu.ec>
2026-06-11 21:02:41 -0500

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,29 @@
+
+#Error sintáctico
+#print("Bienvenidos al curso de Logica de Programacion")
+
+#Error Tiempo ejecución    
+#numero = int(input("Ingrese el numero"))
+#print(numero)
+
+
+#Error Tiempo ejecución    
+#numero = int(4)
+#print (numero)
+
+#Error Semántico
+base = 10
+altura = 5
+#area = base + altura / 2
+#print(area)
+
+#area = (base + altura)/2
+#print (area)
+
+suma = base + altura
+area = suma/2
+print (area)
+
+
+#Error
+#print("Hola mundo")
---
+++ suma.py
@@ -0,0 +1,5 @@
+
+valor1 = float(input("ingreso de un numero:"))
+valor2 = float(input("ingreso de segundo numero"))
+suma = valor1 + valor2
+print (suma)
